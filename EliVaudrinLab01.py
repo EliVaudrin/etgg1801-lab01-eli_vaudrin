ddd86ca7a9937e63b6e73eda51fde7f874e2a2a6
EliVaudrin <70774815+EliVaudrin@users.noreply.github.com>
2020-09-05 11:18:48 -0400

    Add files via upload

---
+++ EliVaudrinLab01.py
@@ -0,0 +1,23 @@
+#ETGG 1801
+#Eli Vaudrin
+#Lab 1-Hello World
+#Date:9/2/2020
+f = "Eli"
+l = "Vaudrin"
+x = 42.68
+y = 28
+print("Lab01 created by: Eli Vaudrin")
+print("Eli", end= ' ')
+print("Vaudrin", end='\n')
+import math
+print("The hypotenuse lenghth is", math.hypot(x, y),"if the other sides are", (x),"and", (y))
+input("Press enter to continue")
+#Square root solver
+n = int(input("Enter any number:"))
+print("The square root of", (n),"is", math.sqrt(n))
+#Body Mass Index Calculator
+height = float(input("Enter your height in meters:"))
+weight = float(input("Enter your weight in in kg:"))
+BMI = weight/ (height**2)
+print("Your BMI is:", (BMI))
+input("Press enter to close")
